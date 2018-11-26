7a5e1092160065ecd94817bafb662579d255bd32
sekugr <sekugr@gmail.com>
2018-11-26 19:48:45 +0300

    color

--- task01.4/task01.4/Program.cs
+++ task01.4/task01.4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace task01._4
@@ -10,7 +11,42 @@ namespace task01._4
     {
         static void Main(string[] args)
         {
+            string str = "C#";
+            int max_x = 119, max_y = 29, max_color = 15;
+            Random r = new Random();
+            for (int i = 0; i<300; ++i)
+            {
+                Console.ForegroundColor = SetColor(r.Next(1, max_color));
+                Console.SetCursorPosition(r.Next(1, max_x), r.Next(1, max_y));
+                Console.Write(str + i);
+                Thread.Sleep(50);
+            }
             Console.ReadKey();
+        }
+
+        private static ConsoleColor SetColor(int v)
+        {
+            switch(v)
+            {
+                case (1): return ConsoleColor.DarkBlue; break;
+                case (2): return ConsoleColor.DarkGreen; break;
+                case (3): return ConsoleColor.DarkCyan; break;
+                case (4): return ConsoleColor.DarkRed; break;
+                case (5): return ConsoleColor.DarkMagenta; break;
+                case (6): return ConsoleColor.DarkYellow; break;
+                case (7): return ConsoleColor.Gray; break;
+                case (8): return ConsoleColor.DarkGray; break;
+                case (9): return ConsoleColor.Blue; break;
+                case (10): return ConsoleColor.Green; break;
+                case (11): return ConsoleColor.Cyan; break;
+                case (12): return ConsoleColor.Red; break;
+                case (13): return ConsoleColor.Magenta; break;
+                case (14): return ConsoleColor.Yellow; break;
+                case (15): return ConsoleColor.White; break;
+                default: return ConsoleColor.Black;
+
+            }
+            //throw new NotImplementedException();
         }
     }
 }
