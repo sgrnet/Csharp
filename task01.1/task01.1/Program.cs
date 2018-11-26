d9272b8ed0c4b07c841ca73136c78a6cb0259acc
sekugr <sekugr@gmail.com>
2018-11-26 19:58:00 +0300

    ini

---
+++ task01.1/task01.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uStr, str = "Я буду усердно делать все задания по C# и заниматься дома";
+            uStr = Console.ReadLine();
+            if (uStr == str)
+            {
+                Console.WriteLine("получишь печеньку");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
