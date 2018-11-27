dbcd24171ec046b2f4827e78ff9ad5337f26742d
sekugr <sekugr@gmail.com>
2018-11-27 11:21:26 +0300

    задание 1,7

---
+++ task01.7/task01.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] ar = new int[10];
+            for (int i = 0; i < 10; ++i)
+            {
+                ar[i] = i + r.Next(1, 50);
+            }
+            
+
+        }
+        static int[] DelArray(int[] array, int[] par)
+        {
+            int[] ResArr = new int[array.Length];
+            return ResArr;
+        }
+    }
+}
